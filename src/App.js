2a881cb0d8246b27479aa197557a2e1de19adefd
vinsonxiao <vinsonxiao@tencent.com>
2024-09-02 17:57:42 +0800

    update proj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logo from "./assets/svg/svgs";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="container d-flex flex-column align-items-center mt-5">
+        {logo}
+        <h1 className="pt-4 mt-5 w-75 fs-1 fw-bold">ReactStrap Template</h1>
+        <p className="pt-2 mt-1 w-75">
+          This innovative template seamlessly integrates React and Bootstrap,
+          providing a streamlined solution for individuals seeking to utilize
+          the benefits of both technologies. By combining these powerful tools,
+          it greatly simplifies the process for those who are already familiar
+          with React and desire to incorporate the styling capabilities of
+          Bootstrap into their projects.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="lead footer mt-2 w-75">
+          Sharp Circle Inc. proudly presents a creation expertly coded by Corey
+          Jimenez
+        </p>
+      </div>
     </div>
   );
 }
